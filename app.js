8a9307e4993d2f95645021b3ccce4dbf508d489f
White Christopher <141556256+0x00whitejsx@users.noreply.github.com>
2024-07-05 11:59:22 +0000

    add file

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-const express  = require('express')
-const app = express()
-const axios = require('axios')
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const port = 8080
 
 
-// https://api.ip2location.io/?key=D74C899063FA3AA7864A20CA0D5E189A&ip=197.210.70.127
-
-
-
-const url = 'https://api.ip2location.io/';
-const apiKey = 'D74C899063FA3AA7864A20CA0D5E189A';
-const ip = '197.210.70.127';
-
-
-
-const port = 8080;
-
-app.get("/api/hello", (req, res) => {
-    const visitor_name1 = req.query.visitor_name;
-
-    axios.get(url, {
-        params: {
-            key: apiKey,
-            ip: ip
-        }
-    })
-    .then(response => {
-        console.log(response.data);
+app.get("/api/hello", async (req, res) => {
+   const ip = req.headers['cf-connecting-ip'] ||
+              req.headers['x-real-ip'] ||
+              req.headers['x-forwarded-for'] ||
+              req.socket.remoteAddress || '';
+              
         res.json({
-            "client_ip": response.data.ip, // The IP address of the requester
-            "location": response.data.city_name, // The city of the requester
-            "greeting": `Hello, ${visitor_name1}, the temperature is 11 degrees Celcius in ${response.data.city_name}`
-        });
-    })
-    .catch(error => {
-        console.error('Error making the API request:', error);
-    });
-   
+            ip:ip
+        })
 });
 
 app.listen(port, () => {
